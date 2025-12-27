c32e982751dc73811d90cc21350247aa0f0f5cd4
torobi <torobi2222@gmail.com>
2025-12-27 15:25:17 +0900

    refactor: Configure import absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+import * as path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   base: '/iOS-Screen-Range/',
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "src"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+    },
+  },
 });
