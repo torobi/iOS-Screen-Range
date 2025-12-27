b1ce5ece885ad6ac7ca4ca615e90afdb2b761836
torobi <torobi2222@gmail.com>
2025-12-27 16:52:41 +0900

    refactor: use getMinModel and getMaxModel utility functions

---
+++ src/lib/modelUtils.ts
@@ -0,0 +1,29 @@
+import type { IPhoneModel } from './iosTypes';
+
+export function getMinModel(
+  models: IPhoneModel[],
+  key: 'width' | 'height',
+): IPhoneModel {
+  const secondKey = key === 'width' ? 'height' : 'width';
+  return models.reduce((min, m) => {
+    if (m.screen[key] < min.screen[key]) return m;
+    if (m.screen[key] === min.screen[key]) {
+      return m.screen[secondKey] < min.screen[secondKey] ? m : min;
+    }
+    return min;
+  }, models[0]);
+}
+
+export function getMaxModel(
+  models: IPhoneModel[],
+  key: 'width' | 'height',
+): IPhoneModel {
+  const secondKey = key === 'width' ? 'height' : 'width';
+  return models.reduce((max, m) => {
+    if (m.screen[key] > max.screen[key]) return m;
+    if (m.screen[key] === max.screen[key]) {
+      return m.screen[secondKey] > max.screen[secondKey] ? m : max;
+    }
+    return max;
+  }, models[0]);
+}
